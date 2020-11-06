ad1927c3c781bf6d22440b9033382f509446cbc2
Alindsey24 <74067610+Alindsey24@users.noreply.github.com>
2020-11-06 13:43:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 """
 MadLibs
-Author: 
-Period/Core:
-
-
+Author: Andrew Lindsey
+Period/Core: 7
 """
+
+name = input('Enter name: ')
+name2 = input('Enter name: ')
+name3 = input('Enter name: ')
+place = input('Enter place: ')
+place2 = input('Enter place: ')
+noun = input('Enter noun: ')
+noun2 = input('Enter noun: ')
+noun3 = input('Enter noun: ')
+noun4 = input('Enter noun: ')
+noun5 = input('Enter noun: ')
+verb = input('Enter verb: ')
+verb2 = input('Enter ing verb: ' )
+verb3 = input('Enter ing verb: ')
+verb4 = input('Enter ed verb: ')
+verb5 = input('Enter verb: ')
+verb6 = input('Enter verb: ')
+verb7 = input('Enter verb: ')
+adj = input('Enter adjective: ')
+adj2 = input('Enter adjective: ')
+adj3 = input('Enter adjective: ')
+number = input('Enter number: ')
+lenti = input('Enter length of time: (ie minute)')
+volspeak = input('Enter volume to speak: (ie bellow, whisper)')
+volspeak2 = input('Enter volume to speak: (ie bellow, whisper)')
+
+print(f"\n\nEveryday always starts the same. Don't ask me why, it's all {name}'s fault. I think today is going to be different though.\n I get to the {place} just as the {noun} {verb}s. Nice. As I look around the {place} I begin to wonder why no one has their {noun2}s out.\n Are we doing something {adj} today? No more {noun3} {verb2} and {verb3}? I can't wait! I took my {noun4} and {verb4}.\n {number} {lenti}s passed. Where is {name}? One of my friends looked over and {volspeak}ed, \"I think he's in the {place2}.\"\n Okay, I thought to myself. Why did that need to be {volspeak}ed? I told my friend to {verb5}. Please. Again, I began to {verb6} for {name} to come back.\n Wait! Someone new entered the {place}. Who's that? \"That's {name2}\" {name3} {volspeak2} from behind me. \"They're {adj3}!\" If they're so {adj3} then why don't they look like it? To be honest, they look like a {noun5}. They're probably just here to {verb7} to us.")
